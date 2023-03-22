5026e191948265b9db8482937f77c0728d211bc1
arther606 <>
2023-03-22 17:17:50 +0800

    es mapping demo

---
+++ elasticsearch-demo/src/main/java/com/yang/elasticsearch/demo/GetIndexMapExample.java
@@ -0,0 +1,48 @@
+package com.yang.elasticsearch.demo;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsRequest;
+import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.cluster.metadata.MappingMetaData;
+import org.elasticsearch.common.collect.ImmutableOpenMap;
+import org.elasticsearch.common.compress.CompressedXContent;
+
+/**
+ * @author admin
+ */
+public class GetIndexMapExample {
+
+    public static void main(String[] args) throws IOException {
+
+        RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+
+        GetMappingsRequest getRequest = new GetMappingsRequest().indices("user-info");
+
+        GetMappingsResponse getResponse = client.indices().getMapping(getRequest, RequestOptions.DEFAULT);
+
+        ImmutableOpenMap<String, ImmutableOpenMap<String, MappingMetaData>> mappings = getResponse.mappings();
+
+        ImmutableOpenMap<String, MappingMetaData> mappingData = mappings.get("user-info");
+        Map<String, Object> sourceMap = mappingData.get("doc").getSourceAsMap();
+
+        LinkedHashMap<String, Object> properties = (LinkedHashMap<String, Object>) sourceMap.get("properties");
+
+        properties.forEach((key, value) -> {
+                    System.out.println(key + " : " + value);
+                    LinkedHashMap<String, Object> map = (LinkedHashMap<String, Object>) value;
+                    System.out.println(map.get("type"));
+                }
+        );
+
+        client.close();
+    }
+}
