6b2e793c41fe2a830a1654c13d2c36146e8db4bc
arther606 <396986654@qq.com>
2023-04-24 14:58:43 +0800

    雪亮kafka没有认证，可以直接获取topic

--- kafka-demo/src/main/java/com/yang/kafka/demo/adminclient/KafkaTopicLister.java
+++ kafka-demo/src/main/java/com/yang/kafka/demo/adminclient/KafkaTopicLister.java
@@ -2,6 +2,7 @@ package com.yang.kafka.demo.adminclient;
 
 import java.util.Properties;
 
+import com.yang.kafka.demo.offset.Commons;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.ListTopicsResult;
 
@@ -9,7 +10,8 @@ public class KafkaTopicLister {
 
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092"); // replace with your kafka bootstrap servers
+//        props.put("bootstrap.servers", "localhost:9092"); // replace with your kafka bootstrap servers
+        props.put("bootstrap.servers", Commons.BOOTSTRAP_SERVERS); // replace with your kafka bootstrap servers
         try (AdminClient client = AdminClient.create(props)) {
             ListTopicsResult topics = client.listTopics();
             topics.names().get().forEach(System.out::println);
