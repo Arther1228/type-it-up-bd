e1623d73e27526d0929354fabea00ca9d4a6ddc9
arther606 <396986654@qq.com>
2023-06-26 11:07:29 +0800

    flink groovy 的demo最大的问题是，不能在运行期间加载类，只能先加载类，找到过先实现一个类，并加载后面再动态继承这个类，从而实现动态加载类的demo，参考其他github项目

--- flink-demo/src/main/java/com/yang/flink/demo/groovy/operator/GroovyDeduplicate.java
+++ flink-demo/src/main/java/com/yang/flink/demo/groovy/operator/GroovyDeduplicate.java
@@ -1,7 +1,5 @@
 package com.yang.flink.demo.groovy.operator;
 
-import com.yang.flink.demo.groovy.util.LoadGroovyClassUtil;
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
@@ -20,13 +18,13 @@ public class GroovyDeduplicate {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        byte[] scriptBytes = Files.readAllBytes(Paths.get("D:\\ylc\\code\\type-it-up-bd\\flink-demo\\src\\main\\java\\com\\yang\\flink\\demo\\groovy\\script\\map.groovy"));
-        String groovyScrpit = new String(scriptBytes, StandardCharsets.UTF_8);
-
         // Kafka配置信息
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "localhost:9092");
         properties.setProperty("group.id", "test");
+
+        byte[] scriptBytes = Files.readAllBytes(Paths.get("D:\\ylc\\code\\type-it-up-bd\\flink-demo\\src\\main\\java\\com\\yang\\flink\\demo\\groovy\\script\\map.groovy"));
+        String groovyScript = new String(scriptBytes, StandardCharsets.UTF_8);
 
         // 创建Kafka数据源
         FlinkKafkaConsumer<String> kafkaSource = new FlinkKafkaConsumer("access_authority", new SimpleStringSchema(), properties);
